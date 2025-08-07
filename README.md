a759df7d0e699ec0a7b966a8e06c2974118d9984
nguyentdk02 <nguyentdk02@example.com>
2025-08-08 04:53:39 +0700

    feat: news translator

---
+++ README.md
@@ -0,0 +1,7 @@
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install requests beautifulsoup4 openai python-dotenv lxml
+
+python3 src/main.py
--- src/main.py
+++ src/main.py
@@ -4,6 +4,28 @@ from bs4 import BeautifulSoup
 from openai import OpenAI
 from dotenv import load_dotenv
 
+new_sites = {
+  "chinese": (
+    "https://cn.chinadaily.com.cn", 
+    "div.Home_content_Item_Text h1 a"
+  ),
+}
+
+def fetch_headlines (language):
+  url, tag = new_sites.get(language, (None, None))
+  if not url:
+    print("language is not supported")
+    return
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text, 'lxml')
+  headlines = [h.getText() for h in soup.select(tag)[:5]]
+  return headlines
+
+def create_prompt (headlines):
+  join_headlines = "\n".join(headlines)
+  prompt = f"Translate the following healines into English: \n{join_headlines}"
+  return prompt
+
 def main():
   load_dotenv()
 
@@ -11,30 +33,21 @@ def main():
   if not api_key:
     print('[error]: can NOT find apikey.')
     return
-  
-  try: 
-    client = OpenAI(api_key=api_key)
-    print('create client success')
-
-    res = client.chat.completions.create(
-      model='gpt-3.5-turbo',
-      messages=[
-        {"role": "system", "content": "you are my assistant"},
-        {"role": "user", "content": "hello user"}
-      ]
-    )
-    print(res.choices[0].message.content)
-  except Exception as e:
-    print (f'[error]: error init client: {e}')
-
-  new_sites = {
-    "china": "chinese site url here",
-    "korean": "korean site url here",
-  }
+    
+  client = OpenAI(api_key=api_key)
 
   user_language = input('what language are you interested in hearing news headlines summarised in?')
-  select_url = new_sites.get(user_language, 'language not supported')
-  print(select_url)
+  selected_headlines = fetch_headlines(user_language)
+  prompt = create_prompt(selected_headlines)
+  res = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+      messages=[{"role": "user", "content": prompt}
+    ],
+    temperature=0.1,
+    max_tokens=200
+  )
+
+  print(res.choices[0].message.content)
 
 if __name__ == "__main__":
   main()
